9bb55ab157dc8294b56cb05ea97ee7dfb68e8292
scolpig <scolpig@gmail.com>
2022-04-06 15:51:13 +0900

    처음 커밋 팀원 여러분 잘 부탁드려요.

---
+++ job01_crawling.py
@@ -0,0 +1,14 @@
+# crawling 작업
+
+# crawling은 각자 진행 하고 빨리 완성되는 코드로 연도를 나눠서 진행하겠습니다.
+# 일단 2022년 개봉작만 진행해 주시고 저장형식은 csv로 하겠습니다.
+# 나머지는 연도별로 나눠서 작업하고 합칠게요.
+# 컬럼명은 ['title', 'reviews']로 통일해주세요.
+# index=False 옵션으로 인덱스 없이 저장해주세요.
+# 파일명은 "reviews_{}.csv".format(연도)로 해주세요.
+# 크롤링한 데이터 파일은 아래 링크로 올려주세요.
+# https://drive.google.com/drive/folders/1NLkgk0zSJlmNwSGq-q1qf6oBUZZLN9AK?usp=sharing
+
+
+
+
