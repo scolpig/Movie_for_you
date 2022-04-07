8b39bde9c24573fb720a2ee80fcba71a4b03ffeb
scolpig <scolpig@gmail.com>
2022-04-07 17:51:56 +0900

    job04_01 preprocessing 01

---
+++ job04_01_preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from konlpy.tag import Okt
+import re
+
+df = pd.read_csv('./crawling_data/reviews_2022.csv')
+print(df.head())
+df.info()
+
+stopwords = pd.read_csv('./crawling_data/stopwords.csv')
+stopwords_list = list(stopwords['stopword'])
+cleaned_sentences = []
+for review in df.reviews:
+    review = re.sub('[^가-힣]', '', review)
+    review_word = review.split(' ')
+
+    words = []
+    for word in review_word:
+        if len(word) > 1:
+            if word not in stopwords_list:
+                words.append(word)
+    cleaned_sentence = ' '.join(words)
+    cleaned_sentences.append(cleaned_sentence)
+df['cleaned_sentences'] = cleaned_sentences
+df = df[['title', 'cleaned_sentences']]
+df.to_csv('./crawling_data/cleaned_review_2018_2022.csv',
+          index=False)
+df.info()
--- job04_preprocessing.py
+++ job04_preprocessing.py
@@ -8,30 +8,32 @@ df.info()
 
 stopwords = pd.read_csv('./crawling_data/stopwords.csv')
 stopwords_list = list(stopwords['stopword'])
-# cleaned_sentences = []
-# for review in df.reviews:
-#     review_word = review.split(' ')
-#     words = []
-#     for word in review_word:
-#         if word not in stopwords_list:
-#             words.append(word)
-#     cleaned_sentence = ' '.join(words)
-#     cleaned_sentences.append(cleaned_sentence)
-# df['cleaned_sentences'] = cleaned_sentences
-# df.to_csv('./crawling_data/cleaned_review_2018_2022.csv',
-#           index=False)
-# df.info()
+cleaned_sentences = []
+for review in df.reviews:
+    review_word = review.split(' ')
+    words = []
+    for word in review_word:
+        if len(word) > 1:
+            if word not in stopwords_list:
+                words.append(word)
+    cleaned_sentence = ' '.join(words)
+    cleaned_sentences.append(cleaned_sentence)
+df['cleaned_sentences'] = cleaned_sentences
+df.to_csv('./crawling_data/cleaned_review_2018_2022.csv',
+          index=False)
+df.info()
 
 okt = Okt()
 count = 0
 cleaned_sentences = []
-for sentence in df.reviews:
+for sentence in df.cleaned_sentences:
     count += 1
     if count % 10 ==0:
         print('.', end='')
     if count % 100 == 0:
         print()
     sentence = re.sub('[^가-힣]', '', sentence)
+
     token = okt.pos(sentence, stem=True)
     # print(token)
     df_token = pd.DataFrame(token,  columns=['word', 'class'])
