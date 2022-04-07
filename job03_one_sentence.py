d0498c8b891ebb73d46b34953f3cb3e3bc25bc10
scolpig <scolpig@gmail.com>
2022-04-07 15:56:37 +0900

    job03 one sentence 01

---
+++ job03_one_sentence.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+df = pd.read_csv('./crawling_data/reviews_2022.csv')
+print(df.head())
+print(df.duplicated().sum())
+df.drop_duplicates(inplace=True)
+df.info()
+
+one_sentences = []
+for title in df['title'].unique():
+    temp = df[df['title']==title]
+    temp = temp['reviews']
+    one_sentence = ' '.join(temp)
+    one_sentences.append(one_sentence)
+df_one_sentences = pd.DataFrame(
+    {'titles':df['title'].unique(), 'reviews':one_sentences})
+print(df_one_sentences.head())
+df_one_sentences.to_csv('./crawling_data/movie_review_onesentence_2018_2022.csv',
+                        index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
