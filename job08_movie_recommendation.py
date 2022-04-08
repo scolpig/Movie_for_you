5d1bda12bcafabd18b8354ddd15ae8133c2e4e79
scolpig <scolpig@gmail.com>
2022-04-08 17:53:32 +0900

    key word 추천

--- job08_movie_recommendation.py
+++ job08_movie_recommendation.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 from scipy.io import mmread, mmwrite
 import pickle
+from gensim.models import Word2Vec
 
 df_reviews = pd.read_csv('./crawling_data/datasets/movie_review_2018_2022.csv')
 df_reviews.info()
@@ -21,13 +22,30 @@ with open('./models/tfidf01.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
 # # 영화 제목을 이용
-# movie_idx = df_reviews[df_reviews['title']=='겨울왕국 2'].index[0]
+# movie_idx = df_reviews[df_reviews['title']=='해피 데스데이 2 유'].index[0]
 # print(movie_idx)
 # 영화 index를 이용
-movie_idx = 469
-print(df_reviews.iloc[movie_idx, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
+# movie_idx = 566
+# print(df_reviews.iloc[movie_idx, 0])
+
+# key_word 이용
+embedding_model = Word2Vec.load('./models/word2vecModel.model')
+key_word = '공포'
+sim_word = embedding_model.wv.most_similar(key_word, topn=10)
+sentence = [key_word] * 11
+words = []
+for word, _ in sim_word:
+    words.append(word)
+for i, word in enumerate(words):
+    sentence += [word] * (10 - i)
+
+sentence = ' '.join(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+
+# cosine_sim = linear_kernel(Tfidf_matrix[movie_idx], Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
+
 print(recommendation)
 
 
