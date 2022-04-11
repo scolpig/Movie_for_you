6c437181c41cbb3b7e23d372af579caa5b4f0334
scolpig <scolpig@gmail.com>
2022-04-11 12:07:24 +0900

    recommendation app 03

--- job11_movie_recommendation_app.py
+++ job11_movie_recommendation_app.py
@@ -20,6 +20,37 @@ class Exam(QWidget, form_window):
         self.setupUi(self)
         self.df_reviews = pd.read_csv(
             './crawling_data/datasets/movie_review_2018_2022.csv')
+        self.Tfidf_matrix = mmread('./models/Tfidf_movie_review01.mtx').tocsr()
+        self.titles = list(self.df_reviews['title'])
+        self.titles.sort()
+        for title in self.titles:
+            self.cmb_titles.addItem(title)
+        self.cmb_titles.currentIndexChanged.connect(
+            self.cmb_titles_slot)
+
+    def cmb_titles_slot(self):
+        title = self.cmb_titles.currentText()
+        recommendation_titles = self.recommend_by_movie_title(title)
+        self.lbl_recommend.setText(recommendation_titles)
+
+    def recommend_by_movie_title(self, title):
+        movie_idx = self.df_reviews[self.df_reviews['title']==title].index[0]
+        cosine_sim = linear_kernel(self.Tfidf_matrix[movie_idx],
+                                   self.Tfidf_matrix)
+        recommendation_titles = self.getRecommendation(cosine_sim)
+        recommendation_titles = '\n'.join(list(recommendation_titles))
+        return recommendation_titles
+
+
+    def getRecommendation(self, cosine_sim):
+        simScore = list(enumerate(cosine_sim[-1]))
+        simScore = sorted(simScore, key=lambda x: x[1],
+                          reverse=True)
+        simScore = simScore[1:11]
+        movieidx = [i[0] for i in simScore]
+        recMovieList = self.df_reviews.iloc[movieidx]
+        return recMovieList.iloc[:, 0]
+
 
 app = QApplication(sys.argv)
 mainWindow = Exam()
